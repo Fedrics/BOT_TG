2d55251ebf2f20939300541aa7f0cada3b51fd5d
Fedrics <143144109+Fedrics@users.noreply.github.com>
2025-09-11 04:51:19 +0500

    firs2

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, send_from_directory
+import os
 
 app = Flask(__name__, static_folder='static', template_folder='webapp')
 
@@ -9,3 +10,7 @@ def index():
 @app.route('/static/<path:filename>')
 def static_files(filename):
     return send_from_directory('static', filename)
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
