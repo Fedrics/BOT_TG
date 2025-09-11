683e19a99957ad5c8c83c75430ff1e4b19c01e77
Fedrics <143144109+Fedrics@users.noreply.github.com>
2025-09-11 20:15:33 +0500

    split bot and web; make src a package; run bot as module

--- src/config.py
+++ src/config.py
@@ -3,14 +3,3 @@ import os
 API_TOKEN = os.environ.get("API_TOKEN", "")
 CRYPTO_PAY_TOKEN = os.environ.get("CRYPTO_PAY_TOKEN", "")
 CRYPTO_PAY_API = "https://pay.crypt.bot/api"
-
-from flask import Flask, render_template
-
-app = Flask(__name__, static_folder='../static', template_folder='../webapp')
-
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-if __name__ == '__main__':
-    app.run(port=8080, debug=True)
